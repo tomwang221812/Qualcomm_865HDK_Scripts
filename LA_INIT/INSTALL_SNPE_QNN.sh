7d83c2e050d87c4a128886420a101a05adb39df6
North Wang <tomwang221812@yahoo.com.tw>
2022-08-12 09:30:40 +0800

    Init

--- LA_INIT/INSTALL_SNPE_QNN.sh
+++ LA_INIT/INSTALL_SNPE_QNN.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export SNPE_DIR='/home/tomwang/snpe-1.64.0.3605'
-export QNN_DIR='/home/tomwang/qnn-v1.16.0.220808064918_38359'
+export SNPE_DIR='/home/north/snpe-1.64.0.3605'
+export QNN_DIR='/home/north/qnn-v1.16.0.220808064918_38359'
 
 # Define binary architeture, OS and hexagon architeture
 export TARGET_ARCH='aarch64'
@@ -22,6 +22,7 @@ export TARGET_SNPE_DSP_LIBS="${TARGET_SNPE_DIR}/dsp"
 export TARGET_QNN_LIBS="${TARGET_QNN_DIR}/target/${TARGET_ARCH}-${TARGET_OS}/lib"
 export TARGET_QNN_HEXAGON_LIBS="${TARGET_QNN_DIR}/target/hexagon-${TARGET_HEXAGON_ARCH}/lib"
 
+# Run adb as root and enable read write
 adb wait-for-device
 adb root
 adb disable-verity
@@ -42,4 +43,8 @@ adb push $QNN_DIR/* $TARGET_QNN_DIR
 # Give executable file access privillage
 adb shell chmod -R +x $TARGET_SNPE_BIN
 adb shell chmod -R +x $TARGET_QNN_BIN
+
+# Test Componets
+# Test SNPE with Platform validator
+adb shell "ADSP_LIBRARY_PATH=${TARGET_SNPE_LIBS}:${TARGET_SNPE_DSP_LIBS} LD_LIBRARY_PATH=${TARGET_SNPE_LIBS}:${TARGET_SNPE_DSP_LIBS} ./${TARGET_SNPE_BIN}/snpe-platform-validator --runtime all"
 
